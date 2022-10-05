548b9ffbb978d50497a6f79aa4a2bf8b20056f99
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-05 16:47:54 -0300

    feat: req 00 - creates migration file for TEAMS table

---
+++ app/backend/src/database/migrations/20221005193656-create-teams.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.creatTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('teams');
+  }
+};
