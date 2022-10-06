812d1e66bc86e936e0e54fec591fd78d079760ec
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-06 12:04:35 -0300

    refact: req XX - middlewares restructured

--- app/backend/src/database/middlewares/bodyValidations.ts
+++ app/backend/src/database/middlewares/bodyValidations.ts
@@ -1,18 +1,30 @@
 import { NextFunction, Request, Response } from 'express';
-import ILogin from '../interfaces/ILogin';
+import IUser from '../interfaces/ILogin';
 
-const loginValidation = (req: Request, res: Response, next: NextFunction) => {
-  const { email, password } = req.body as ILogin;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: 'All fields must be filled' });
-  }
-
-  if (password.length < 6) {
-    return res.status(400).json({ message: 'Password must have at least 6 characters' });
-  }
-
-  next();
+const loginValidation = {
+  loginNotEmpty: (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body as IUser;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  },
+  loginValidateEmail: (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body as IUser;
+    const validateEmailInputRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isplayerEmailValid = validateEmailInputRegex.test(email);
+    if (!isplayerEmailValid) {
+      return res.status(400).json({ message: '"email" must be a valid email' });
+    }
+    next();
+  },
+  loginFieldsLength: (req: Request, res: Response, next: NextFunction) => {
+    const { password } = req.body as IUser;
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must have at least 6 characters' });
+    }
+    next();
+  },
 };
 
 export default loginValidation;
