0464231ea5a714f1121390bca9846785b6b7b9c5
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-05 21:24:47 -0300

    feat: req 01 - creates Teams  models

---
+++ app/backend/src/database/models/Teams.model.ts
@@ -0,0 +1,41 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model {
+  // public <campo>!: <tipo>;
+  public id!: number;
+  public teamName!: string;
+}
+
+Teams.init({
+  // ... Campos
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Teams, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Teams, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Teams.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Teams.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
