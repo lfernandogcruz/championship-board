3d77532173ef9b074ea5d0fc56fa0ef37edf0d21
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-07 13:00:15 -0300

    feat: req 03 - creates the /login rout itself, with proper middlewares and call for controllers

--- app/backend/src/database/routes/login.route.ts
+++ app/backend/src/database/routes/login.route.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import loginValidation from '../middlewares/bodyValidations';
+import UsersController from '../controllers/Users.controller';
+import UsersService from '../services/Users.service';
 
-const router = Router();
+const usersService = new UsersService();
+const usersController = new UsersController(usersService);
 
-router.post(
+const loginRouter = Router();
+
+loginRouter.post(
   '/login',
   loginValidation.loginNotEmpty,
   loginValidation.loginValidateEmail,
   loginValidation.loginFieldsLength,
+  usersController.findOne,
 );
 
-export default router;
+export default loginRouter;
