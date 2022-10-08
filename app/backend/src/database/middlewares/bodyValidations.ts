ff3126b8146ff27d59b64b6a46e37a77fe7f762e
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-07 22:05:16 -0300

    feat: req 03 - creates middleware for token and password validation at /login POST route

--- app/backend/src/database/middlewares/bodyValidations.ts
+++ app/backend/src/database/middlewares/bodyValidations.ts
@@ -1,13 +1,15 @@
+import * as bcrypt from 'bcryptjs';
 import { NextFunction, Request, Response } from 'express';
 import IUser from '../interfaces/IUser';
+import User from '../models/User.model';
 
 const loginValidation = {
   loginNotEmpty: (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body as IUser;
-    if (!email || !password) {
+    if (!email || !password || email === undefined || password === undefined) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
-    next();
+    return next();
   },
   loginValidateEmail: (req: Request, res: Response, next: NextFunction) => {
     const { email } = req.body as IUser;
@@ -16,14 +18,25 @@ const loginValidation = {
     if (!isplayerEmailValid) {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
-    next();
+    return next();
   },
   loginFieldsLength: (req: Request, res: Response, next: NextFunction) => {
     const { password } = req.body as IUser;
     if (password.length < 6) {
       return res.status(400).json({ message: 'Password must have at least 6 characters' });
     }
-    next();
+    return next();
+  },
+  loginEmailAndPasswordValidation: async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body as IUser;
+    const response = await User.findOne({ where: { email }, raw: true });
+    if (!response) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+    if (!bcrypt.compareSync(password, response.password)) {
+      throw new Error('Sorry, incorrect password');
+    }
+    return next();
   },
 };
 
