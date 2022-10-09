6662c557e8fe3b31f69b722f57c84455f24add81
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-09 01:14:52 -0300

    refact: middlewares inactivated due to missfunction and validations moved to Services layer

--- app/backend/src/database/middlewares/bodyValidations.ts
+++ app/backend/src/database/middlewares/bodyValidations.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcryptjs';
+// import * as bcrypt from 'bcryptjs';
 import { NextFunction, Request, Response } from 'express';
 import IUser from '../interfaces/IUser';
 import User from '../models/User.model';
@@ -7,9 +7,11 @@ const loginValidation = {
   loginNotEmpty: (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body as IUser;
     if (!email || !password || email === undefined || password === undefined) {
+      // console.log('<><><><><><><><> loginNotEmpty - missing fields');
       return res.status(400).json({ message: 'All fields must be filled' });
     }
-    return next();
+    // console.log('loginNotEmpty');
+    next();
   },
   loginValidateEmail: (req: Request, res: Response, next: NextFunction) => {
     const { email } = req.body as IUser;
@@ -18,25 +20,33 @@ const loginValidation = {
     if (!isplayerEmailValid) {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
-    return next();
+    // console.log('loginValidateEmail');
+    next();
   },
   loginFieldsLength: (req: Request, res: Response, next: NextFunction) => {
     const { password } = req.body as IUser;
     if (password.length < 6) {
       return res.status(400).json({ message: 'Password must have at least 6 characters' });
     }
-    return next();
+    // console.log('loginFieldsLength');
+    next();
   },
   loginEmailAndPasswordValidation: async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body as IUser;
+    const { email } = req.body as IUser;
+    // const { email, password } = req.body as IUser;
     const response = await User.findOne({ where: { email }, raw: true });
+    // console.log('response', response);
     if (!response) {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
-    if (!bcrypt.compareSync(password, response.password)) {
-      throw new Error('Sorry, incorrect password');
-    }
-    return next();
+    // const passwordCheck = bcrypt.compareSync(password, response.password);
+    // // console.log('passwordCheck', passwordCheck);
+    // if (!passwordCheck) {
+    //   // console.log('Sorry, incorrect password');
+    //   throw new Error('Sorry, incorrect password');
+    // }
+    // console.log('loginEmailAndPasswordValidation');
+    next();
   },
 };
 
