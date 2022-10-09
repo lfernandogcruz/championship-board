652c1cc3beb30c952c531aad534a20efaf805158
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-09 01:22:17 -0300

    chenges the structure of controller so the token isn't generated in this layer anymore

--- app/backend/src/database/controllers/Users.controller.ts
+++ app/backend/src/database/controllers/Users.controller.ts
@@ -1,9 +1,5 @@
 import { Request, Response } from 'express';
-import { sign } from 'jsonwebtoken';
 import UsersService from '../services/Users.service';
-import 'dotenv/config';
-
-const JWT_SECRET = process.env.JWT_SECRET as string;
 
 class UsersController {
   constructor(private usersService = new UsersService()) {}
@@ -14,8 +10,7 @@ class UsersController {
     if (!response) {
       return res.status(404).json({ message: 'Sorry, the princess is in another castle!' });
     }
-    const token = sign({ email }, JWT_SECRET, { expiresIn: '8d', algorithm: 'HS256' });
-    return res.status(200).json({ token });
+    return res.status(200).json({ token: response });
   }
 }
 
