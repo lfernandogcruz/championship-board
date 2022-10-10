c9b30c02713920d0cee34e991944d35a9895ea88
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-10 10:40:44 -0300

    refact: req 03, 05, 07, 09, 11 - updates routes at app file

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -12,6 +12,8 @@ class App {
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.use('/login', routes.login);
+    this.app.use('/teams', routes.teams);
+    this.app.use('/matches', routes.matches);
   }
 
   private config():void {
