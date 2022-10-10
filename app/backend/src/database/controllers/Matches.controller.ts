4af338598a71a9f81ef2da92d357b2bced1838a5
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-10 18:15:45 -0300

    feat: req 23 - creates a POST gateway for Matches at controllers layer

--- app/backend/src/database/controllers/Matches.controller.ts
+++ app/backend/src/database/controllers/Matches.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
+import teamIdValidation from '../helpers/teamIdValidation';
 import MatchesService from '../services/Matches.service';
+import constants from '../helpers/constants';
+import IMatch from '../interfaces/IMatch';
 
 class MatchesController {
   constructor(private matchesService = new MatchesService()) {}
@@ -7,9 +10,7 @@ class MatchesController {
   public async findAll(_req: Request, res: Response): Promise<Response> {
     const response = await this.matchesService.findAll();
     if (!response) {
-      return res
-        .status(404)
-        .json({ message: 'Sorry, the princess is in another castle!' });
+      return res.status(404).json({ message: constants.error404Message });
     }
     return res.status(200).json(response);
   }
@@ -18,11 +19,25 @@ class MatchesController {
     const { id } = req.params;
     const response = await this.matchesService.findById(Number(id));
     if (!response) {
-      return res
-        .status(404)
-        .json({ message: 'Sorry, the princess is in another castle!' });
+      return res.status(404).json({ message: constants.error404Message });
     }
     return res.status(200).json(response);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { homeTeam, awayTeam } = req.body;
+    const teamAreValid = await teamIdValidation(homeTeam, awayTeam);
+    if (!teamAreValid) {
+      return res.status(404).json({ message: 'Team not found' });
+    }
+    const { homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+    const newMatch = {
+      homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress };
+    const response = await this.matchesService.create(newMatch) as IMatch;
+    if (!response) {
+      return res.status(404).json({ message: constants.error404Message });
+    }
+    return res.status(201).json(response);
   }
 }
 
