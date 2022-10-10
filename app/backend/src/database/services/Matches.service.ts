a7320db5f4f04dbeac328019c4a14991da253c24
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-10 14:10:11 -0300

    refact: adds 'includes' attribute on findAll query

--- app/backend/src/database/services/Matches.service.ts
+++ app/backend/src/database/services/Matches.service.ts
@@ -1,19 +1,29 @@
 import IMatch from '../interfaces/IMatch';
 import Matches from '../models/Matches.model';
+import Teams from '../models/Teams.model';
 
 class MatchesService {
   public db = Matches;
 
-  public findAll = async (): Promise<IMatch[] | unknown> => {
-    const matches = await this.db.findAll();
-    if (!matches) return null as unknown;
-    return matches as unknown as IMatch[];
+  public findAll = async (): Promise<IMatch[] | null> => {
+    const matches = await this.db.findAll({ include: [{
+      model: Teams,
+      as: 'teamHome' as string,
+      attributes: { include: ['teamName'], exclude: ['id'] },
+    }, {
+      model: Teams,
+      as: 'teamAway' as string,
+      attributes: { include: ['teamName'], exclude: ['id'] },
+    },
+    ] });
+    if (!matches) return null;
+    return matches as IMatch[];
   };
 
-  public findById = async (id: number): Promise<IMatch | unknown> => {
+  public findById = async (id: number): Promise<IMatch | null> => {
     const match = await this.db.findByPk(id);
-    if (!match) return null as unknown;
-    return match as unknown as IMatch;
+    if (!match) return null;
+    return match as IMatch;
   };
 }
 
