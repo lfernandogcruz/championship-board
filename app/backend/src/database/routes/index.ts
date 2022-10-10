10054de12e266914fd08fd544b2f10e97c8e3562
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-10 10:52:55 -0300

    feat: req XX - creates new routes for Teams and Matches while waiting for monday morning's help to fix previous crashes

--- app/backend/src/database/routes/index.ts
+++ app/backend/src/database/routes/index.ts
@@ -1,5 +1,9 @@
 import loginRouter from './login.route';
+import teamsRouter from './teams.route';
+import matchesRouter from './matches.route';
 
 export default {
   login: loginRouter,
+  teams: teamsRouter,
+  matches: matchesRouter,
 };
