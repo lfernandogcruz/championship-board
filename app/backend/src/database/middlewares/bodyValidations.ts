33a933c4ae8fdb13f3b9c2fe4bb158626c213a37
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-10 10:47:35 -0300

    style: console.logs clean-up

--- app/backend/src/database/middlewares/bodyValidations.ts
+++ app/backend/src/database/middlewares/bodyValidations.ts
@@ -7,10 +7,8 @@ const loginValidation = {
   loginNotEmpty: (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body as IUser;
     if (!email || !password || email === undefined || password === undefined) {
-      console.log('<><><><><><><><> loginNotEmpty - missing fields');
       return res.status(400).json({ message: 'All fields must be filled' });
     }
-    console.log('loginNotEmpty');
     next();
   },
   loginValidateEmail: (req: Request, res: Response, next: NextFunction) => {
@@ -20,7 +18,6 @@ const loginValidation = {
     if (!isplayerEmailValid) {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
-    // console.log('loginValidateEmail');
     next();
   },
   loginFieldsLength: (req: Request, res: Response, next: NextFunction) => {
@@ -28,24 +25,18 @@ const loginValidation = {
     if (password.length < 6) {
       return res.status(400).json({ message: 'Password must have at least 6 characters' });
     }
-    // console.log('loginFieldsLength');
     next();
   },
   loginEmailAndPasswordValidation: async (req: Request, res: Response, next: NextFunction) => {
-    // const { email } = req.body as IUser;
     const { email, password } = req.body as IUser;
     const response = await User.findOne({ where: { email }, raw: true });
-    // console.log('response', response);
     if (!response) {
       return res.status(401).json({ message: 'Incorrect email or password' });
     }
     const passwordCheck = bcrypt.compareSync(password, response.password);
-    // console.log('passwordCheck', passwordCheck);
     if (!passwordCheck) {
-    // console.log('Sorry, incorrect password');
       throw new Error('Sorry, incorrect password');
     }
-    console.log('loginEmailAndPasswordValidation');
     next();
   },
 };
