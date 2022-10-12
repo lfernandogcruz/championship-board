d9482eb55f524daba7624db242ba74c2b9e6b371
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-12 16:38:00 -0300

    feat/refact/fix: req 29+ - corrects minor logic adjustments for /homne and /away and implements leaderboard's calculation for general board

--- app/backend/src/database/helpers/leaderboardFunctions.ts
+++ app/backend/src/database/helpers/leaderboardFunctions.ts
@@ -42,9 +42,9 @@ const LeaderboardFunctions = {
     const totalPoints = matchesList
       .filter((match) => match[mainTeam] === team.id)
       .reduce((acc, match) => {
-        if (match[mainTeamGoals] > match[opponentGoals]) return acc + 3; // win
-        if (match[mainTeamGoals] === match[opponentGoals]) return acc + 1; // draw
-        return acc + 0; // loss
+        if (match[mainTeamGoals] > match[opponentGoals]) return (acc + 3); // win
+        if (match[mainTeamGoals] === match[opponentGoals]) return (acc + 1); // draw
+        return (acc + 0); // loss
       }, 0);
     return totalPoints;
   },
@@ -124,8 +124,41 @@ const LeaderboardFunctions = {
     || b.totalVictories - a.totalVictories
     || b.goalsBalance - a.goalsBalance
     || b.goalsFavor - a.goalsFavor
-    || b.goalsOwn + a.goalsOwn);
-    return sortedLeaderboard as ILeaderboard[];
+    || b.goalsOwn - a.goalsOwn);
+    return sortedLeaderboard;
+  },
+
+  teamObjConstructorGeneral: (team: ITeam, matchesList: IMatch[]) => {
+    const teamObjectPt1 = {
+      name: team.teamName,
+      totalPoints: LeaderboardFunctions.totalPoints(team, matchesList, 'home')
+        + LeaderboardFunctions.totalPoints(team, matchesList, 'away'),
+      totalGames: LeaderboardFunctions.totalGames(team, matchesList, 'home')
+        + LeaderboardFunctions.totalGames(team, matchesList, 'away'),
+      totalVictories: LeaderboardFunctions.totalVictories(team, matchesList, 'home')
+        + LeaderboardFunctions.totalVictories(team, matchesList, 'away'),
+    };
+    const teamObjectPt2 = LeaderboardFunctions.teamObjConstructorGeneral2(team, matchesList);
+    const innerObj = LeaderboardFunctions.teamObjConstructor2(
+      teamObjectPt2.goalsFavor,
+      teamObjectPt2.goalsOwn,
+      teamObjectPt1.totalPoints,
+      teamObjectPt1.totalGames,
+    );
+    return { ...teamObjectPt1, ...teamObjectPt2, ...innerObj };
+  },
+  teamObjConstructorGeneral2: (team: ITeam, matchesList: IMatch[]) => {
+    const teamObjectPt2 = {
+      totalDraws: LeaderboardFunctions.totalDraws(team, matchesList, 'home')
+        + LeaderboardFunctions.totalDraws(team, matchesList, 'away'),
+      totalLosses: LeaderboardFunctions.totalLosses(team, matchesList, 'home')
+        + LeaderboardFunctions.totalLosses(team, matchesList, 'away'),
+      goalsFavor: LeaderboardFunctions.goalsFavor(team, matchesList, 'home')
+        + LeaderboardFunctions.goalsFavor(team, matchesList, 'away'),
+      goalsOwn: LeaderboardFunctions.goalsOwn(team, matchesList, 'home')
+        + LeaderboardFunctions.goalsOwn(team, matchesList, 'away'),
+    };
+    return teamObjectPt2;
   },
 };
 
