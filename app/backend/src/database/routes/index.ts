af0181a598d7f13b67e5993f2e876215ba092e2c
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-11 22:40:27 -0300

    feat: import/export leaderboards' route onto routes index

--- app/backend/src/database/routes/index.ts
+++ app/backend/src/database/routes/index.ts
@@ -1,9 +1,11 @@
 import loginRouter from './login.route';
 import teamsRouter from './teams.route';
 import matchesRouter from './matches.route';
+import leaderboardRouter from './leaderboard.route';
 
 export default {
   login: loginRouter,
   teams: teamsRouter,
   matches: matchesRouter,
+  leaderboard: leaderboardRouter,
 };
