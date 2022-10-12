5033fca478fa9ec487f24a155d89ca0da27816bc
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-12 16:38:58 -0300

    feat/refact/fix: req 29+ - corrects minor logic adjustments for /homne and /away and implements leaderboard's calculation for general board at Service layer

--- app/backend/src/database/services/Leaderboard.service.ts
+++ app/backend/src/database/services/Leaderboard.service.ts
@@ -7,21 +7,64 @@ class LeaderboardService {
   public teams = Teams;
   public matches = Matches;
 
+  private includeHome: {
+    model: typeof Matches;
+    as: string;
+    where: { inProgress: boolean; };
+    attributes: string[];
+  };
+
+  private includeAway: {
+    model: typeof Matches;
+    as: string;
+    where: { inProgress: boolean; };
+    attributes: string[];
+  };
+
+  constructor() {
+    this.includeHome = {
+      model: this.matches,
+      as: 'teamHome',
+      where: { inProgress: false },
+      attributes: ['homeTeamGoals', 'awayTeamGoals'],
+    };
+
+    this.includeAway = {
+      model: this.matches,
+      as: 'teamAway',
+      where: { inProgress: false },
+      attributes: ['homeTeamGoals', 'awayTeamGoals'],
+    };
+  }
+
   public homeFindAll = async (): Promise<ILeaderboard[] | null> => {
-    const teamsList = await this.teams.findAll({
-      include: [
-        {
-          model: this.matches,
-          as: 'teamHome',
-          where: { inProgress: false },
-          attributes: ['homeTeamGoals', 'awayTeamGoals'],
-        },
-      ],
-    });
-    const matchesList = await this.matches.findAll();
+    const teamsList = await this.teams.findAll();
+    const matchesList = await this.matches.findAll({ where: { inProgress: false } });
     const leaderboard = teamsList
       .map((team) => helpers
         .teamObjConstructor(team, matchesList, 'home')) as unknown as ILeaderboard[];
+    if (!leaderboard) return null;
+    const sortedLeaderboard = helpers.sortLeaderboard(leaderboard);
+    return sortedLeaderboard;
+  };
+
+  public awayFindAll = async (): Promise<ILeaderboard[] | null> => {
+    const teamsList = await this.teams.findAll();
+    const matchesList = await this.matches.findAll({ where: { inProgress: false } });
+    const leaderboard = teamsList
+      .map((team) => helpers
+        .teamObjConstructor(team, matchesList, 'away')) as unknown as ILeaderboard[];
+    if (!leaderboard) return null;
+    const sortedLeaderboard = helpers.sortLeaderboard(leaderboard);
+    return sortedLeaderboard;
+  };
+
+  public generalFindAll = async (): Promise<ILeaderboard[] | null> => {
+    const teamsList = await this.teams.findAll();
+    const matchesList = await this.matches.findAll({ where: { inProgress: false } });
+    const leaderboard = teamsList
+      .map((team) => helpers
+        .teamObjConstructorGeneral(team, matchesList)) as unknown as ILeaderboard[];
     if (!leaderboard) return null;
     const sortedLeaderboard = helpers.sortLeaderboard(leaderboard);
     return sortedLeaderboard;
