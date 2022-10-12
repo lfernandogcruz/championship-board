08ad19d25df39d65a3f9aac9e369ffc267d340aa
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-10-12 16:39:27 -0300

    feat/refact/fix: req 29+ - corrects minor logic adjustments for /homne and /away and implements leaderboard's calculation for general board at Controller layer

--- app/backend/src/database/controllers/Learderboard.controller.ts
+++ app/backend/src/database/controllers/Learderboard.controller.ts
@@ -9,6 +9,16 @@ class LeaderboardController {
     const leaderboard = await this.leaderboardService.homeFindAll();
     return res.status(200).json(leaderboard);
   }
+
+  public async awayFindAll(_req: Request, res: Response): Promise<Response> {
+    const leaderboard = await this.leaderboardService.awayFindAll();
+    return res.status(200).json(leaderboard);
+  }
+
+  public async generalFindAll(_req: Request, res: Response): Promise<Response> {
+    const leaderboard = await this.leaderboardService.generalFindAll();
+    return res.status(200).json(leaderboard);
+  }
 }
 
 export default LeaderboardController;
